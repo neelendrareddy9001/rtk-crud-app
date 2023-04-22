c7f78c27fc3499c5d2b2c110ae3228d4641880be
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-22 15:01:30 +0200

    Update UserInfo.tsx

--- src/pages/UserInfo.tsx
+++ src/pages/UserInfo.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useParams, Link } from "react-router-dom";
+import "./UserInfo.css";
 
 const UserInfo = () => {
+  const { id } = useParams();
   return (
-    <div>UserInfo</div>
-  )
-}
+    <div style={{ marginTop: "150px" }}>
+      <div className="card">
+        <div className="card-header">
+          <p>User Contact Detail</p>
+        </div>
+        <div className="container">
+          <strong>ID: </strong>
+          <span>1</span>
+          <br />
+          <br />
+          <strong>Name: </strong>
+          <span>Test</span>
+          <br />
+          <br />
+          <strong>Email: </strong>
+          <span>test@gmail.com</span>
+          <br />
+          <br />
+          <strong>Contact: </strong>
+          <span>775675673</span>
+          <br />
+          <br />
+          <Link to="/">
+            <button className="btn btn-edit">Go Back</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default UserInfo
+export default UserInfo;
