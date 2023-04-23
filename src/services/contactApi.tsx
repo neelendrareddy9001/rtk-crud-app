ca182d78d2d438e184d38bc1a1ff1b2823748b4d
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-23 23:47:49 +0200

    Add files via upload

---
+++ src/services/contactApi.tsx
@@ -0,0 +1,32 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import {Contact} from '../models/contac.model';
+
+
+export const contactApi = createApi({
+    reducerPath : "contactApi",
+    baseQuery : fetchBaseQuery({baseUrl : " http://localhost:5000/"}),
+    tagTypes: ["Contacts"],
+    endpoints : (builder) => ({
+        contacts : builder.query<Contact[], void>({
+            query : () => "/contacts",
+            providesTags: ["Contacts"]
+        }),
+        addContact : builder.mutation<{}, Contact>({
+            query : (contact) => ({
+                url : "/contacts",
+                method : "POST",
+                body : contact,
+            }),
+            invalidatesTags: ["Contacts"],
+        }),
+        deleteContact : builder.mutation<void, string>({
+            query : (id) => ({
+                url : `/contacts/${id}`,
+                method : "DELETE"
+            }),
+            invalidatesTags: ["Contacts"]
+        })
+    }),
+});
+
+export const {useContactsQuery, useAddContactMutation, useDeleteContactMutation} = contactApi;
